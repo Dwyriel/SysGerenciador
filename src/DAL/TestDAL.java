7626f74a5a01d9d3498c4eda28f96cc8188022fc
Lucas-Gds <61030955+Lucas-Gds@users.noreply.github.com>
2021-01-23 20:15:02 -0300

    Connection 0.1

--- src/DAL/TestDAL.java
+++ src/DAL/TestDAL.java
@@ -10,16 +10,16 @@ public class TestDAL extends Conexao{
 	public static User user = new User();
 	public static String password;
 	public static List<Institution> institutions = new ArrayList<Institution>();
-//	static {
-//		for(int i = 0; i < 3; i++) {
-//			Institution institution = new Institution("institution"+i, "Institution " + i);
-//			List<Lesson> lessons = new ArrayList();
-//			lessons.add(new Lesson("aula1", "Aula 1", institution));
-//			lessons.add(new Lesson("aula2", "Aula 2", institution));
-//			institution.setClasses(lessons);
-//			institutions.add(institution);
-//		}
-//	}
+	static {
+		for(int i = 0; i < 3; i++) {
+			Institution institution = new Institution("institution"+i, "Institution " + i);
+			List<Lesson> lessons = new ArrayList();
+			lessons.add(new Lesson("aula1", "Aula 1", institution));
+			lessons.add(new Lesson("aula2", "Aula 2", institution));
+			institution.setClasses(lessons);
+			institutions.add(institution);
+		}
+	}
 	public User insert(User usuario) {
         try {
             super.abrir();
