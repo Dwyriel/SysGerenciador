3c865608ca2816c95c375496b80a4b20a16bdc7b
Dwyriel <dwyriel333@hotmail.com>
2021-01-24 19:09:48 -0300

    LessonDAL WIP

--- src/DAL/UserDAL.java
+++ src/DAL/UserDAL.java
@@ -20,7 +20,6 @@ public class UserDAL {
 			ResultSet id = statement.getGeneratedKeys();
 			if (id.next())
 				user.setId(id.getInt(1));
-
 			return user;
 		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
@@ -70,12 +69,12 @@ public class UserDAL {
 			String query2 = "DELETE FROM teacher_lessons WHERE teacher_id = "+id;
 			String query3 = "DELETE FROM lesson_students WHERE student_id = "+id;
 			String query4 = "DELETE FROM users WHERE id = "+id;
-			//TODO update lessons so it removes teacher_id / sets it -1 
+			String query5 = "UPDATE lessons SET teacher_id = -1 WHERE teacher_id = "+ id;
 			statement.addBatch(query1);
 			statement.addBatch(query2);
 			statement.addBatch(query3);
 			statement.addBatch(query4);
-
+			statement.addBatch(query5);
 			statement.executeBatch();
 
 			return true;
@@ -87,7 +86,7 @@ public class UserDAL {
 		}
 	}
 
-	public static User get(int id) {
+	public static User getUser(int id) {
 		try {
 			Connection connection = Conexao.getConnection();
 
